bbb956198fe904ccc80a8d34e7d2aa32f4c9f00a
Evridiki Lam <evridiki.lam@gmail.com>
2025-12-15 18:49:28 +0200

    added functionality

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 let num1 = Number(prompt("Give me the first number"));
+let num2 = Number(prompt("Give me the second number"));
+let op = prompt("Give me the operator");
+let result;
+
+if (op === '+'){
+    result = num1 + num2;
+}
+else if (op === '-') {
+    result = num1 - num2;
+}
+else if (op === '*') {
+    result = num1*num2;
+}
+else if (op=== '/' && num2!=0) {
+    result = num1/num2;
+}
+alert(`The result is ${result}`);
